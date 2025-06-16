8a24c936965157f554ed81ce83fec56cc6e0082b
creepereye1204 <iostream1204@gmail.com>
2025-06-16 07:06:52 +0000

    셀러리 추가

--- baord/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from .tasks import example_task
-
-def board_async_view(request):
-    # 예시: 비동기 작업 큐에 추가
-    result = example_task.delay(1, 2)
-    return JsonResponse({'task_id': result.id, 'status': 'queued'})
